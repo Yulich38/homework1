1f43b1aa9f5cc06d70b186beb0506078ed7e09ec
Yulia <julja123julja@gmail.com>
2023-07-18 18:31:23 +0300

    jgg

---
+++ seminar1/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 2 Написать программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+a = 5; b = 7 -> max = 7
+a = 2; b = 10 -> max = 10
+a = - 9; b = -3 -> max = -3
+*/
+
+// Console.WriteLine("Введите первое число:");
+// int a =  Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a<b)
+// {
+// System.Console.WriteLine("Большее число:" + b + "Меньшее число:"+a);
+// }
+// else
+// {
+//     System.Console.WriteLine("Большее число: {a}, меньшее число: {b}");
+// }
+
+
+/*
+Задача 4 Написать программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+
+2,3,7 -> 7
+44,5,78 -> 78
+22,3,9 -> 22
+*/
+
+// Console.WriteLine("Введите первое число:");
+// int a =  Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+// int b =  Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число:");
+// int c =  Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+// if (max < b) max = b;
+// if (max < c) max = c;
+
+// System.Console.WriteLine("Максимальное число из этих чисел =" + max);
+
+/*
+Задача 6 Напишите программу, которая на вход принимает число и выдает, является ли число четным (делиться ли оно на два без остатка)
+
+4 -> да
+- 3 -> да
+7 -> да
+*/
+
+// Console.WriteLine("Введите число:");
+// int a =  Convert.ToInt32(Console.ReadLine());
+
+// if (a % 2 ==0)
+// { 
+// Console.WriteLine("четное");
+// }
+// else
+// {
+// Console.WriteLine("нечетное");
+// }
+
+/*
+Задача 8  Напишите программу, которая на вход принимаем число (N), а на выходе показывает  все четные числа от 1 до N
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+// Console.WriteLine("Введите число N");
+// int n =  Convert.ToInt32(Console.ReadLine());
+
+// for (int i=1; i<=n; i++)
+// { 
+// if (i % 2 == 0) 
+// Console.WriteLine(i + ",");
+// }
